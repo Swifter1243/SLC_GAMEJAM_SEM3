40fbb85d4a72767682547b8087b1a9014ad77062
Frautcres <Frautcres@FRAUTCRES>
2024-10-23 17:38:11 -0400

    fix the key

--- Assets/Scripts/Game/KeyTask.cs
+++ Assets/Scripts/Game/KeyTask.cs
@@ -2,24 +2,17 @@ using UnityEngine;
 
 public class KeyTask : Task
 {
-    private SpriteRenderer[] _spriteRenderers;
-    
-    private void Awake() //Should be as early as possible or serialized beforehand
-    {
-        _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == Constants.LAYER_PLAYER)
         {
-            foreach (SpriteRenderer sprite in _spriteRenderers) sprite.enabled = false;
-            CompleteTask(); //FIXME: doesn't this mean that this task can be completed twice?
+            gameObject.SetActive(false);
+            CompleteTask();
         }
     }
 
     protected override void _Reset()
     {
-        foreach (SpriteRenderer sprite in _spriteRenderers) sprite.enabled = true;
+        gameObject.SetActive(true);
     }
 }
