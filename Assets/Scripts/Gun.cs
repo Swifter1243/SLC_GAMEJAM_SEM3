b57b39992aa1b1d00d97447ae206561d3c11b9c0
Swifter <61858676+Swifter1243@users.noreply.github.com>
2024-10-23 10:26:41 -0400

    player works now YIPPEEEEEEEEEEE

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,10 +9,22 @@ public class Gun : MonoBehaviour
     public FaceCursor faceCursor;
     public UnityEvent onFire;
 
+    bool _fired = false;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            _fired = true;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (_fired)
+        {
+            _fired = false;
             Fire();
         }
     }
