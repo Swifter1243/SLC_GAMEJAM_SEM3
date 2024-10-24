c4e81fb17a7865b8b383fefc12e44d0a7d3107bd
Swifter <61858676+Swifter1243@users.noreply.github.com>
2024-10-24 14:14:36 -0400

    destroy bullet after seconds

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -6,23 +6,24 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float destroyAfterSeconds = 5;
     private Player _player;
 
     public void Initialize(Player player)
     {
         player.bullets.Add(this);
+        Destroy(gameObject, destroyAfterSeconds);
         _player = player;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy();
+        Destroy(gameObject);
     }
 
-    public void Destroy()
+    private void OnDestroy()
     {
         _player.bullets.Remove(this);
-        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -31,6 +32,5 @@ public class Bullet : MonoBehaviour
         {
             other.gameObject.GetComponent<TargetTask>().Hit();
         }
-
     }
 }
