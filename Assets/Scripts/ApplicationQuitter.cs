a5685fd8447471751595fdc17416acdf1609eaa8
Swifter <swifter1243@gmail.com>
2024-10-24 18:05:27 -0400

    make menu buttons functional

---
+++ Assets/Scripts/ApplicationQuitter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ApplicationQuitter : MonoBehaviour
+{
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
