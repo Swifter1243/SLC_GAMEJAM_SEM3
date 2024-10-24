6a1a365a2e2e5cdf7b2c4b6b7b1e1e4a7b57095f
Swifter <swifter1243@gmail.com>
2024-10-23 21:46:12 -0400

    switch passage pair

---
+++ Assets/Scripts/Passage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Passage : MonoBehaviour, IResettable
+{
+    public void Reset()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switch : MonoBehaviour, IResettable
+{
+    private bool _on = false;
+    public Transform leverPivot;
+    public UnityEvent onTurnedOn;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == Constants.LAYER_PLAYER)
+        {
+            TurnOn();
+        }
+
+        if (other.gameObject.layer == Constants.LAYER_BULLET)
+        {
+            other.gameObject.GetComponent<Bullet>().Destroy();
+            TurnOn();
+        }
+    }
+
+    private void TurnOn()
+    {
+        if (_on)
+        {
+            return;
+        }
+
+        leverPivot.rotation = Quaternion.Euler(0, 0, -45);
+        onTurnedOn.Invoke();
+        _on = true;
+    }
+
+    public void Reset()
+    {
+        leverPivot.rotation = Quaternion.Euler(0, 0, 45);
+        _on = false;
+    }
+}
