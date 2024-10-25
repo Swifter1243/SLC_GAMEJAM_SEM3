a199e1abd6afb5f93de2d2800a2b423dd3900f80
Swifter <swifter1243@gmail.com>
2024-10-25 06:15:47 -0400

    bullet made better

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -8,6 +8,7 @@ public class Bullet : MonoBehaviour
     public Rigidbody2D rb;
     public float destroyAfterSeconds = 5;
     private Player _player;
+    public GameObject particlePrefab;
 
     public void Initialize(Player player)
     {
@@ -23,6 +24,7 @@ public class Bullet : MonoBehaviour
 
     private void OnDestroy()
     {
+        Instantiate(particlePrefab, transform.position, Quaternion.identity);
         _player.bullets.Remove(this);
     }
 
